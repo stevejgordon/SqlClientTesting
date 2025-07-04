65fefd4eb83de5b7bdcd20e0466c1ae126e383bf
Steve Gordon <sgordon@hotmail.co.uk>
2025-07-04 17:01:24 +0100

    Additional testing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace SqlClientTesting.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             using (var connection = new SqlConnection("Server=localhost;Database=TestDb;User Id=SA;Password=Password1!;"))
             {
@@ -27,23 +29,77 @@ namespace SqlClientTesting.Controllers
                     }
                 }
 
-                using (var command2 = new SqlCommand("ProcedureName", connection)
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM Inventory";
+                    using (System.Data.IDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (reader.Read())
+                        {
+                        }
+                    }
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM Inventory";
+                    using (System.Data.IDataReader reader = await command.ExecuteReaderAsync(CancellationToken.None))
+                    {
+                        while (reader.Read())
+                        {
+                        }
+                    }
+                }
+
+                using (var command = new SqlCommand("ProcedureName", connection)
                 {
                     CommandType = System.Data.CommandType.StoredProcedure
                 })
                 {
-                    using (System.Data.IDataReader reader = command2.ExecuteReader())
+                    using (System.Data.IDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            for (int i = 0; i < reader.FieldCount; i++)
-                            {
-                                Response.Write($"{reader.GetName(i)}: {reader.GetValue(i)}<br/>");
-                            }
-                            Response.Write("<hr/>");
                         }
                     }
                 }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO Inventory VALUES (10, 'apple', 200)";
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO Inventory VALUES (10, 'apple-async', 200)";
+                    await command.ExecuteNonQueryAsync();
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO Inventory VALUES (10, 'apple-async', 200)";
+                    await command.ExecuteNonQueryAsync(CancellationToken.None);
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT Count(*) from Inventory";
+                    command.ExecuteScalar();
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT Count(*) from Inventory";
+                    await command.ExecuteScalarAsync();
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT Count(*) from Inventory";
+                    await command.ExecuteScalarAsync(CancellationToken.None);
+                }
+
                 connection.Close();
 
                 Response.End();
